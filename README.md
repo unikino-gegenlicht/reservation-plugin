baedaa21c6240eefe3bdb6070cac2cd4f1beb19e
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-03-18 12:58:53 +0100

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+![Unikino GEGENLICHT Logo](https://gegenlicht.net/wp-content/themes/gegenlicht/img/logo/gegenlicht_logo_gelb_schwarz.svg)
+# Reservation Plugin for WordPress
+
+![GitHub](https://img.shields.io/github/license/unikino-gegenlicht/reservation-plugin?style=for-the-badge)
+
+## Description
+This plugin is used to allow visitors of our homepage to reserve tickets for our movies. 
---
+++ reservation-plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Reservation Plugin
+ *
+ * @package           Reservation_Plugin
+ * @author            Jan Eike Suchard
+ * @copyright         2023 Jan Eike Suchard
+ * @license           MIT
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Reservierungs-Plugin
+ * Plugin URI:        https://github.com/unikino-gegenlicht/reservation-plugin
+ * Description:       This plugin allows the Unikino GEGENLICHT to manage reservations for their events.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      8.0
+ * Author:            Jan Eike Suchard
+ * Author URI:        https://suchard.cloud
+ * Text Domain:       reservation-plugin
+ * License:           MIT
+ * License URI:       https://spdx.org/licenses/MIT.txt
+ * Update URI:        false
+ */
