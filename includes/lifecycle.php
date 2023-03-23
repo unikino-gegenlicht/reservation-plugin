db324433f1d5e1a9ca2513a16ec777420601bc5d
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-03-23 20:31:10 +0100

    ♻️ moving the lifecycle hooks own class

---
+++ includes/lifecycle.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Fired during plugin lifecycle events
+ *
+ * @link       https://suchard.cloud
+ * @since      1.0.0
+ *
+ * @package    Reservation_Plugin
+ * @subpackage Reservation_Plugin/includes
+ */
+
+/**
+ * Functions for the lifecycle of the plugin
+ *
+ * This class defines all code necessary to run during the plugin's lifecycle
+ *
+ * @since      1.0.0
+ * @package    Reservation_Plugin
+ * @subpackage Reservation_Plugin/includes
+ * @author     Jan Eike Suchard <jan+WordPressPlugin@suchard.cloud>
+ */
+class Plugin_Lifecycle {
+
+	/**
+	 * Fired during plugin activation.
+	 *
+	 * This function creates some tables in the database to allow the reservations to be stored and managed from the
+	 * WordPress backend.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate(): void {
+		// create a table for storing the reservations
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'movie_reservations';
+		$query = "CREATE TABLE IF NOT EXISTS `$table_name` (
+    				id int(9) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    				movieID int(9) NOT NULL REFERENCES wp_posts(ID),
+    				personName varchar(255) NOT NULL,
+    				email text,
+    				seats int(9) NOT NULL,
+    				pickedUp bool DEFAULT FALSE
+					) $charset_collate";
+		dbDelta($query);
+		// add options to use a default limit on reservable seats
+		add_option("reservations_default_limit", 50);
+	}
+
+	/**
+	 * Called to deactivate the plugin
+	 *
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate(): void {
+
+	}
+
+	/**
+	 * Fired during plugin deinstallation.
+	 *
+	 * This function deletes the created database table
+	 *
+	 * @since    1.0.0
+	 */
+	public static function uninstall(): void {
+		global $wpdb;
+		$table_name = $wpdb->prefix . 'movie_reservations';
+		$query = "DROP TABLE $table_name";
+		dbDelta($query);
+		delete_option("reservations_default_limit");
+	}
+
+}
--- reservation-plugin.php
+++ reservation-plugin.php
@@ -34,25 +34,36 @@ const RESERVATION_PLUGIN_VERSION = '1.0.0';
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/activator.php
+ * This action is documented in includes/lifecycle.php
  */
 function activate_reservation_plugin(): void {
-	require_once plugin_dir_path(__FILE__) . 'includes/activator.php';
-	Reservation_Plugin_Activator::activate();
+	require_once plugin_dir_path(__FILE__) . 'includes/lifecycle.php';
+	Plugin_Lifecycle::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/deactivator.php
+ * This action is documented in includes/lifecycle.php
  */
 function deactivate_reservation_plugin(): void {
-	require_once  plugin_dir_path(__FILE__) . 'includes/deactivator.php';
-	Reservation_Plugin_Deactivator::deactivate();
+	require_once plugin_dir_path( __FILE__ ) . 'includes/lifecycle.php';
+	Plugin_Lifecycle::deactivate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ *
+ * This function just calls the uninstallation method from the Plugin_Lifecycle class
+ */
+function uninstall_reservation_plugin(): void {
+	require_once plugin_dir_path(__FILE__). 'includes/lifecycle.php';
+	Plugin_Lifecycle::uninstall();
 }
 
 // register the two functions as activation and deactivation hooks
 register_activation_hook(__FILE__, 'activate_reservation_plugin');
 register_deactivation_hook(__FILE__, 'deactivate_reservation_plugin');
+register_uninstall_hook(__FILE__, 'uninstall_reservation_plugin');
 
 // now include the core plugin class which defines the hooks for public and
 // admin areas, and internationalization
