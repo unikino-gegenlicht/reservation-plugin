50d548357401c45ee3f83b652aeeed7d582d2243
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-03-23 09:01:16 +0100

    🚧 adding deactivator

---
+++ includes/deactivator.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://gegenlicht.net
+ * @since      1.0.0
+ *
+ * @package    Reservation_Plugin
+ * @subpackage Reservation_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Reservation_Plugin
+ * @subpackage Reservation_Plugin/includes
+ * @author     Jan Eike Suchard <jan-eike.suchard@magenta.de>
+ */
+class Reservation_Plugin_Deactivator {
+
+	/**
+	 * Called to deactivate the plugin
+	 *
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate(): void {
+
+	}
+
+}
